6d53a84abbedccddcaaccf522b1f112306bee32c
SergeyEskov1 <eskovs193@gmail.com>
2024-09-05 20:26:01 +0300

    Update Main.java

--- calcul-string/src/Main.java
+++ calcul-string/src/Main.java
@@ -1,13 +1,105 @@
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        String exp = scn.nextLine();
+        exp = exp.replace("\"", "");
+        String[] data;
+        char action;
+
+        if (exp.contains("+")) {
+            data = exp.split("\\+");
+            action = '+';
+        } else if (exp.contains("-")) {
+            data = exp.split("-");
+            action = '-';
+        } else if (exp.contains("*")) {
+            data = exp.split("\\*");
+            action = '*';
+        } else if (exp.contains("/")) {
+            data = exp.split("/");
+            action = '/';
+        } else {
+            throw new Exception("Некорректный знак дейстивия");
+        }
+
+        if (action == '*' || action == '/') {
+            try
+            {
+                Integer.parseInt(data[1]);
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("Строчку можно делить или умножать на число");
+            }
+        }
+        String result = "";
+        if (action == '+') {
+            result = sumString(data[0], data[1]);
+        } else if (action == '/'){
+            result = divideString(data[0], data[1]);
+        }
+        else if (action == '*') {
+            result = multiplyString(data[0], data[1]);
+        } else if (action == '-') {
+            result = takeItAwayString(data[0], data[1]);
+        }
+
+        printInQuote(result);
+    }
+        public static String takeItAwayString(String firstStr, String secondStr) {
+           String takeItAwayresult = "";
+            int index = firstStr.indexOf(secondStr);
+            if (index == -1) {
+                printInQuote(firstStr);
+            }else{
+                takeItAwayresult = firstStr.substring(0, index);
+                takeItAwayresult +=firstStr.substring(index+secondStr.length());
+            }
 
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        String num1 = "Hi World!";
-        String num2 = "World!";
+    return takeItAwayresult;
+    }
+    public static String divideString(String firstStr, String secondStr) {
 
-        String num3 = num1.concat(-num2);
-        System.out.println(num3);
+        String divideResult = "";
+        int newLen = firstStr.length()/Integer.parseInt(secondStr);
+        divideResult = firstStr.substring(0, newLen);
+        return divideResult;
+    }
+
+    public static String sumString(String firstStr,String secondStr)
+    {
+        return firstStr + secondStr;
+    }
+
+    public static String multiplyString(String firstStr, String secondStr)
+    {
+        int multiplier = Integer.parseInt(secondStr);
+        String multiplyResult = "";
+        for (int i = 0; i < multiplier; i++) {
+            multiplyResult += firstStr;
+
+        }
+        return multiplyResult;
+    }
+
+    public static void printInQuote(String text)
+    {
+        String result = trimString(text,10);
+        System.out.println("\""+result+"\"");
+    }
+
+    public static String trimString(String text, int count)
+    {
+        String result = text;
+        if(result.length() >= count)
+        {
+            result = result.substring(0,count - 1);
+            result += "...";
+        }
+        return result;
     }
 }
+ 
